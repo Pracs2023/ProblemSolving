d6abcb8c6bbab02d0c5ac5ee054a7f24750eac76
Pracs2023 <pracs2023@gmail.com>
2023-05-23 21:35:35 +0530

    ReducedString

--- ReducedString.java
+++ ReducedString.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Test {
+public class ReducedString {
     public static void main(String[] args) {
             Scanner sc= new Scanner(System.in);
             String s = sc.nextLine();
