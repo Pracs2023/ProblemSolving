99288cad4fa19808c31dc31ed95af53736e5fb29
Pracs2023 <pracs2023@gmail.com>
2023-05-23 21:33:02 +0530

    Test

---
+++ Test.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+            Scanner sc= new Scanner(System.in);
+            String s = sc.nextLine();
+            StringBuilder sb = new StringBuilder(s);
+            int i = 0;
+            int end = sb.length() - 1;
+
+            while (i < end && sb.length() != 2) {
+                if (sb.charAt(i) == sb.charAt(i + 1)) {
+                    sb.deleteCharAt(i);
+                    sb.deleteCharAt(i);
+                    end -= 2;
+                    if (i > 0)
+                        i--;
+                } else
+                    i++;
+            }
+
+            if (sb.length() == 0 || sb.length() == 2 && sb.charAt(0) == sb.charAt(1))
+                System.out.println("Empty string");
+else
+            System.out.println(sb.toString());
+        }
+    }
+
+
